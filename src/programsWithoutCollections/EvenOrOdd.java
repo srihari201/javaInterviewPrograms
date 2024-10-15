61c5956c8a05b8076d6de9ff9804c29b20c3a594
srihari <srihariadada19978@gmail.com>
2024-10-15 20:25:25 +0530

    commiting to the complete project

---
+++ src/programsWithoutCollections/EvenOrOdd.java
@@ -0,0 +1,24 @@
+package programsWithoutCollections;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the Number:");
+		int number=scanner.nextInt();
+		
+		if(number%2==0) {
+			System.out.println("Entering into if condition");
+			System.out.println( number + " is even");	
+		}
+		else {
+			System.out.println(number + "is odd");
+		}
+		
+
+	}
+
+}
